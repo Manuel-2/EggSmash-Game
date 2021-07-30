8a2c685bf1d4e0e0a45ab0b1823f443ec9cdafea
Manuel <25lopezm25@gmail.com>
2021-07-29 21:46:15 -0600

    se añadio un fondo dinamico y el titulo del juego asi todo bonito

---
+++ Assets/Scripts/backgroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rigidbody;
+    [SerializeField] float speed;
+    [SerializeField] Vector2 movementDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.velocity = movementDirection.normalized * speed;
+    }
+
+    private void Update()
+    {
+        if(transform.position.x <= -3)
+        {
+            transform.position = new Vector3(0,0,0); 
+        }
+    }
+
+}
