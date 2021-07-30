e978c726aa3e9f057239bc0142b86f60ef919524
Manuel <25lopezm25@gmail.com>
2021-07-29 20:16:50 -0600

    se añadio la funcionalidad para cambiar el volumen del juego

--- Assets/Scripts/CameraInitialConfiguration.cs
+++ Assets/Scripts/CameraInitialConfiguration.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class CameraInitialConfiguration : MonoBehaviour
 {
+    public static CameraInitialConfiguration sharedInstance;
+
     private void Awake()
     {
+        if(sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
         Application.targetFrameRate = 60;
+    }
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
     }
 }
---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour
+{ 
+    [SerializeField] Slider slider;
+    [SerializeField] float sliderValue;
+    [SerializeField] Image muteIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("AudioVolume", 0.7f);
+        AudioListener.volume = slider.value;
+        CheckMute();
+    }
+
+    public void ChangeSlider(float value)
+    {
+        sliderValue = value;
+        PlayerPrefs.SetFloat("AudioVolume", sliderValue);
+        AudioListener.volume = Mathf.Pow(sliderValue, 2);
+        CheckMute();
+    }
+
+    public void CheckMute()
+    {
+        if (sliderValue == 0)
+        {
+            muteIcon.enabled = true;
+        }
+        else
+        {
+            muteIcon.enabled = false;
+        }
+    }
+}
