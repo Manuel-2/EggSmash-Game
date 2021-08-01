a7c5112571991ce0e8af3d9a8bf28b579aeb0353
Manuel <25lopezm25@gmail.com>
2021-08-01 11:03:59 -0600

    se añadieron tranciciones entre las escenas

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] Animator transitionAnim;
+    [SerializeField] float transitionDuration;
+    [SerializeField] string transitionTrigger;
+    [SerializeField] int gameScene;
+    [SerializeField] int menuScene;
+
+    public void LoadScene()
+    {
+        StartCoroutine(LoadLevel(gameScene));
+    }
+
+    public void ReturnMenu()
+    {
+        StartCoroutine(LoadLevel(menuScene));
+    }
+
+    IEnumerator LoadLevel(int Scene)
+    {
+        transitionAnim.SetTrigger(transitionTrigger);
+        yield return new WaitForSeconds(transitionDuration);
+        SceneManager.LoadScene(Scene);
+    }
+}
